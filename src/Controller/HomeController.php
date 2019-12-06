dceb4d5ccb458ce370b72c2fc0a36f1906d0b1a0
Darlene <darlene.dadou@gmail.com>
2019-12-06 13:42:36 +0100

    first version  fixture

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Langage;
+use App\Entity\Projet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class HomeController extends AbstractController
 {
@@ -12,8 +15,35 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $entityManager=$this->getDoctrine()->getManager();
+        $projetRepo = $entityManager->getRepository(Projet::class);
+        $langageRepo = $entityManager->getRepository(Langage::class);
+        $projet = $projetRepo->findAll();
+
+        // if(empty($projet)){
+            // $p1 = new Projet();
+            // $p1->setNom("Stack MEAN");
+            // $p1->setLiens(array("https://github.com/DadouO/node-back-sf-store","https://github.com/DadouO/angular-sf-store"));
+            // $n = $langageRepo->findOneBy(array('nom'=>'node.js'));
+            // $a = $langageRepo->findOneBy(array('nom'=>'Angular 8'));
+            // $p1->addLangage($n);
+            // $p1->addLangage($a);
+
+
+            // $entityManager->persist($p1);
+
+            // $entityManager->flush();
+        // }
+        // $entite = $langageRepo->find(1);
+        // $entityManager->remove($entite);
+        // $entityManager->flush();
+
+        // $entite = $projetRepo->find(1);
+        // $entityManager->remove($entite);
+        // $entityManager->flush();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'projets' => $projetRepo->findAll(),
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,51 +3,125 @@
 namespace App\DataFixtures;
 
 use App\Entity\Langage;
+use App\Entity\Projet;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+
+    // function tab(array $arr, String $t, ObjectManager $m){
+    //     for($count = 0; $count<count($arr); $count++)
+    //     {
+    //     $nom = $arr[$count];
+    //     $type= $t;
+
+    //     $langage = new langage();
+    //     $langage->setNom($nom);
+    //     $langage->setType($type);
+
+    //     $m->persist($langage);
+        
+    //     }
+    // }
+
+    public function load(ObjectManager $m)
     {
 
         $back = array("node.js", "Java", "Python", "PHP", "Kotlin");
         $front=array("Angular 8", "React", "JavaScript", "HTML5", "CSS3");
         $bd=array("MYSQL","Oracle","PL/SQL","SPARQL","MongoDB");
+        // $pnom=array()
+        // https://github.com/claimskg/claimskg-statistical-observatory
+        // tab($back,"Backend");
+        // tab($front,"Frontend");
+        // tab($bd,"Base de données");
 
-        // for($count = 0; $count<10; $count++)
-        // {
-        // $titre = "Titre Fixture";
-        // $description = "Description Fixture";
-        // $note = mt_rand(1,5);
+        $p1 = new Projet();
+        $p1->setNom("Stack MEAN");
+        $p1->setLiens(array("https://github.com/DadouO/node-back-sf-store","https://github.com/DadouO/angular-sf-store"));
+        $m->persist($p1);
 
-        // $book = new Book();
-        // $book->setTitre($titre);
-        // $book->setDescription($description);
-        // $book->setNote($note);
+        $p2 = new Projet();
+        $p2->setNom("Python");
+        $p2->setLiens(array("https://github.com/claimskg/claimskg-statistical-observatory"));
+        $m->persist($p2);
 
-        // $manager->persist($book);
+        $p3 = new Projet();
+        $p3->setNom("Stack MERN");
+        $p3->setLiens(array("https://github.com/DadouO/node-back-sf-store","https://github.com/DadouO/react-sf-store"));
+        $m->persist($p3);
+
+        $p4 = new Projet();
+        $p4->setNom("Symfony");
+        $p4->setLiens(array("https://github.com/DadouO/symfony-technos-perso"));
+        $m->persist($p4);
+
+            for($count = 0; $count<count($back); $count++)
+            {
+            $nom = $back[$count];
+            $type= "Backend";
+    
+            $langage = new langage();
+            $langage->setNom($nom);
+            $langage->setType($type);
+    
+            if($nom=="node.js"){
+                $langage->setProjet($p1);
+            }
+            if($nom=="Python"){
+                $langage->setProjet($p2);
+            }
+            if($nom=="PHP"){
+                $langage->setProjet($p4);
+            }
+            $m->persist($langage);
+            
+            }
         
-        // }
+            for($count = 0; $count<count($front); $count++)
+            {
+            $nom = $front[$count];
+            $type= "Frontend";
+    
+            $langage = new langage();
+            $langage->setNom($nom);
+            $langage->setType($type);
+    
+            if($nom=="Angular 8"){
+                $langage->setProjet($p1);
+            }
+            if($nom=="React"){
+                $langage->setProjet($p3);
+            }
+            $m->persist($langage);
+            
+            }
 
-        // $product = new Product();
-        // $manager->persist($product);
+            for($count = 0; $count<count($bd); $count++)
+            {
+            $nom = $bd[$count];
+            $type= "Base de données";
+    
+            $langage = new langage();
+            $langage->setNom($nom);
+            $langage->setType($type);
 
-        $manager->flush();
+            if($nom=="MongoDB"){
+                $langage->setProjet($p1);
+            }
+            if($nom=="SPARQL"){
+                $langage->setProjet($p2);
+            }
+            $m->persist($langage);
+            
+            }
+
+          
+            // $p1
+
+        $m->flush();
     }
 
-    public function tab(array $arr, String $t){
-        for($count = 0; $count<count($arr); $count++)
-        {
-        $nom = $arr[$count];
-        $type= $t;
 
-        $langage = new langage();
-        $langage->setNom($nom);
-        $langage->setType($type);
-
-        $manager->persist($langage);
-        
-        }
-    }
 }
