5b3444f14515b161d0bceb24a4b08f64bacac5b2
Darlene <darlene.dadou@gmail.com>
2019-12-07 17:44:52 +0100

    responsive design langage

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addStyleEntry('globalStyle', './assets/global/import.scss')
+    .addStyleEntry('langageStyle', './assets/composant/langage/import.scss')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
