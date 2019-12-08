ed99e9bdce02aee0bd5c8d1f8c39ac0a42ef25d1
Darlene <darlene.dadou@gmail.com>
2019-12-08 12:21:45 +0100

    migration and populate meetups table

--- src/DataFixtures/MeetupFixtures.php
+++ src/DataFixtures/MeetupFixtures.php
@@ -6,7 +6,7 @@ use App\Entity\Meetups;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class MeetupFixtures extends Fixture
 {
     public function load(ObjectManager $m)
     {
